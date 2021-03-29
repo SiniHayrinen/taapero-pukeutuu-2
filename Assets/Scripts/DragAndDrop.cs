cea6baa875e524e1dff24641dc91d199855d440f
SiniHayrinen <sini.hayrinen@gmail.com>
2021-03-29 21:18:50 +0300

    Added functionality and audio to the drag and drop
    Now it checks if the cat food is dropped on the cat's head or not, and if so, hides the food and plays audio.

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -7,6 +7,8 @@ public class DragAndDrop : MonoBehaviour
 	
 	bool moveAllowed;
 	Collider2D col;
+	public Transform goalPlace;
+	public AudioSource crunch;
 	
     // Start is called before the first frame update
     void Start()
@@ -44,7 +46,53 @@ public class DragAndDrop : MonoBehaviour
 			{
 				moveAllowed = false;
 				Debug.Log("ended");
+
+				// If the cat food is on top of cat's face
+				if (Mathf.Abs(transform.position.x - goalPlace.position.x) <= 1.5f && Mathf.Abs(transform.position.y - goalPlace.position.y) <= 1.5f) 
+				{
+					Debug.Log("maali");
+
+					// Hides the cat food that was eaten
+					this.gameObject.SetActive(false);
+
+					crunch.Play();
+				}
 			}
 		}
     }
 }
+
+
+/* -----
+	private void OnMouseUp()
+	{
+		isBeingHeld = false;
+		
+		if (Mathf.Abs(transform.position.x - goalPlace.position.x) <= 1.5f && Mathf.Abs(transform.position.y - goalPlace.position.y) <= 1.5f) 
+		{
+			if (!locked) {
+				transform.position = new Vector2(goalPlace.position.x, goalPlace.position.y);
+				audioSource.Play();
+				locked = true;
+				gameBehaviourScript.vaatteet1 = gameBehaviourScript.vaatteet1 + 1;
+
+				if (gameBehaviourScript.vaatteet1==4) {
+
+					//vaatteet2Transform.position = new Vector3(2.42f,1.1f,0);
+					vaatteet2Transform.DOMove(new Vector3(2.42f, 1.1f, 0), 1);
+				}
+				// Skene vaihtuu
+				if (gameBehaviourScript.vaatteet1==8) {
+					//Hahmo siirtyy ovesta ulos
+					DOVirtual.DelayedCall(1, WalkOut);
+				}
+			}
+			
+		}
+		else {
+
+			transform.DOMove(new Vector2(initialPosition.x, initialPosition.y), 1);
+			//transform.position = new Vector2(initialPosition.x, initialPosition.y);
+		}
+	}
+ */
